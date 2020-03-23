6225c79fb6120b963b9dc58c2ca4a4c10f93da9f
Caruki <caruki91@posteo.de>
2020-03-23 15:35:56 +0100

    Add write function

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
-const [command, key] = process.argv.slice(2);
+const [command, key, value] = process.argv.slice(2);
 
 function get() {
   try {
     const passwordsJSON = fs.readFileSync('./db.json', 'utf8');
     const passwords = JSON.parse(passwordsJSON);
     console.log(key, passwords[key]);
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
 }
 
 function set() {
-  console.log('Called SET', key);
+  try {
+    const passwordsJSON = fs.readFileSync('./db.json', 'utf8');
+    const passwords = JSON.parse(passwordsJSON);
+    passwords[key] = value;
+    fs.writeFileSync('./db.json', JSON.stringify(passwords));
+  } catch (erroe) {
+    console.error(error);
+  }
 }
 
 if (command === 'get') {
