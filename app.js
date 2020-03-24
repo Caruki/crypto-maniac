a813bcb81f0c1fc542c15351f3be7bfb7d359db1
Caruki <caruki91@posteo.de>
2020-03-24 16:48:14 +0100

    Add hashing of master password

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const { get, set, unset } = require('./lib/commands');
+const { get, set, unset, reset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
 const { askForPassword, askForMasterPassword } = require('./lib/questions');
+const { verifyHash } = require('./lib/crypto');
 const { readMasterPassword } = require('./lib/dbaccess');
 
 async function run() {
   const inputMasterPassword = await askForMasterPassword();
-  if (inputMasterPassword === readMasterPassword()) {
-    if (command === 'get') {
-      get(key);
-    } else if (command === 'set') {
-      const password = await askForPassword(key);
-      set(key, password);
-    } else if (command === 'unset') {
-      unset(key);
-    } else {
-      console.log('Unknown command');
-    }
-  } else {
+
+  const masterPassword = readMasterPassword();
+
+  if (!verifyHash(inputMasterPassword, masterPassword)) {
     console.log('Fuck Off!');
     return;
+  }
+
+  if (command === 'reset') {
+    return reset(inputMasterPassword);
+  }
+
+  if (command === 'get') {
+    get(key);
+  } else if (command === 'set') {
+    const password = await askForPassword(key);
+    set(key, password);
+  } else if (command === 'unset') {
+    unset(key);
+  } else {
+    console.log('Fuck Off!');
+    return 'Unknown command';
   }
 }
 
