d4981b08e50bf4be8c268fed9ea6053a64790dce
Caruki <caruki91@posteo.de>
2020-03-24 14:50:03 +0100

    Add master pw validation

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const { get, set, unset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
-const { askForPassword } = require('./lib/questions');
+const { askForPassword, askForMasterPassword } = require('./lib/questions');
+const { readMasterPassword } = require('./lib/passwords');
 
 async function run() {
-  if (command === 'get') {
-    get(key);
-  } else if (command === 'set') {
-    const password = await askForPassword(key);
-    set(key, password);
-  } else if (command === 'unset') {
-    unset(key);
-  } else {
-    console.log('Unknown command');
+  const input = await askForMasterPassword();
+  if (input === readMasterPassword()) {
+    if (command === 'get') {
+      get(key);
+    } else if (command === 'set') {
+      const password = await askForPassword(key);
+      set(key, password);
+    } else if (command === 'unset') {
+      unset(key);
+    } else {
+      console.log('Unknown command');
+    }
   }
 }
 
